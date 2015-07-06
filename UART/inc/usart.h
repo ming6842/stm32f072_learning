7cd639128d54e6d9b6a01e843ca9fe8aceaeca12
ming6842 <ming6842@hotmail.com>
2015-07-06 12:31:45 +0800

    Clean up error

--- UART/inc/usart.h
+++ UART/inc/usart.h
@@ -1,9 +1,13 @@
 #ifndef FILE_USART_H
 #define FILE_USART_H
 
+#include <string.h>
 
 void USART1_Initialization(void);
 void USART3_Initialization(void);
 void USART1_IRQHandler(void);
+void USART3_dma_init(void);
+void usart3_dma_send_text(uint8_t *s);
+void USART3_4_IRQHandler(void);
 
 #endif
